afdd02d9a5368438dedf11b4d45079b130db8c9e
CuteITGuy <grisetroseciel@gmail.com>
2016-01-09 19:18:16 +0700

    First commit

---
+++ RoutedEventArgs/RoutedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+
+namespace CB.Wpf
+{
+    public class RoutedEventArgs<T>: RoutedEventArgs
+    {
+        #region  Constructors & Destructor
+        public RoutedEventArgs() { }
+
+        public RoutedEventArgs(T value): this(value, null, null) { }
+
+        public RoutedEventArgs(T value, RoutedEvent routedEvent): this(value, routedEvent, null) { }
+
+        public RoutedEventArgs(T value, RoutedEvent routedEvent, object source): base(routedEvent, source)
+        {
+            Value = value;
+        }
+        #endregion
+
+
+        #region  Properties & Indexers
+        public T Value { get; set; }
+        #endregion
+    }
+}
